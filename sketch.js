b77cc7415112cc9069b9912ba9486da819589061
SuperShaun2008 <78541658+SuperShaun2008@users.noreply.github.com>
2021-05-08 18:19:21 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,67 @@
+var canvas, backgroundImage;
+
+var gameState = 0;
+var playerCount;
+var allPlayers;
+var distance = 0;
+var database;
+
+var form, player, game;
+var cars, car1, car2, car3, car4;
+var track, ground, car1_image, car2_image, car3_image, car4_image;
+var passedFinish, finishedPlayers = 0;
+
+function preload() {
+  track = loadImage("images/track.jpg")
+  ground = loadImage("images/ground.png")
+  car1_image = loadImage("images/car1.png")
+  car2_image = loadImage("images/car2.png")
+  car3_image = loadImage("images/car3.png")
+  car4_image = loadImage("images/car4.png")
+  f1_image = loadImage("images/f1.png")
+  gold_image = loadImage("images/gold.png")
+  bronze_image = loadImage("images/bronze.png")
+  silver_image = loadImage("images/silver.png")
+  s = loadSound("sound/sliding.mp3")
+}
+
+
+function setup() {
+  canvas = createCanvas(displayWidth, displayHeight);
+  database = firebase.database();
+
+  xVel = 0
+  yVel = 0
+
+  obstacles = createGroup();
+
+
+  for (i = 0; i < 5; i++) {
+    x = random(200, 950)
+    y = random(-height * 4, height - 300)
+    f1 = createSprite(x, y)
+    f1.addImage(f1_image)
+    obstacles.add(f1)
+  }
+  game = new Game();
+  game.getState();
+  game.start();
+}
+
+
+function draw() {
+  background(200,200,255)
+  if (playerCount === 4 && finishedPlayers === 0) {
+    game.update(1);
+  }
+  if (gameState === 1) {
+    clear();
+    game.play();
+  }
+  if (finishedPlayers === 4) {
+    game.update(2)
+  }
+  if (gameState === 2 && finishedPlayers === 4) {
+    game.displayRanks()
+  }
+}
